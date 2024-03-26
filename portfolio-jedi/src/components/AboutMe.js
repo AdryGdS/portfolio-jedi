ebd38d8cd5d9ab70afc3291c3de112dbfead5f54
Adriely Gogora <99200566+AdryGdS@users.noreply.github.com>
2024-03-26 17:49:58 +0000

    Criação de React, criação de functions em javascript e modificações de css

---
+++ portfolio-jedi/src/components/AboutMe.js
@@ -0,0 +1,10 @@
+function AboutMe(){
+    return(
+        <div className="card">
+            <h1>Sobre mim</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+        </div>
+    )
+}
+
+export default AboutMe
---
+++ portfolio-jedi/src/components/Image.js
@@ -0,0 +1,6 @@
+function Image(){
+    return(
+        <img src="https://media.licdn.com/dms/image/D4D03AQGSKdGg8j7gwA/profile-displayphoto-shrink_400_400/0/1705521149164?e=1717027200&v=beta&t=uqjAuev_QkjFow8-mSvDVG1_8Z5BO4Pb-lSVoYHe5TQ" alt="Foto de perfil" className="image"/>
+    )
+}
+export default Image
